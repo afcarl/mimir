64a1916dfecbd1aa023a11cc4b6e5cadc8be991c
Rory MacQueen <rorymacqueen@gmail.com>
2017-06-28 18:56:26 -0400

    Replace sklearn package with statsmodel
    Since only with statsmodel do we get the standard error figure
    for each coefficient.

--- mimir.py
+++ mimir.py
@@ -1,12 +1,13 @@
 import numpy as np
-from sklearn import linear_model
+import statsmodels.api as sm
 
 # Parameters
 
 PORTFOLIO_CUTOFF = 0.2
 NUM_T_PERIODS = 80
 NUM_ASSETS = 1000
-THRESHOLD = 0.01
+DELTA_THRESHOLD = 0.01
+ERROR_THRESHOLD = 0.1
 
 with open('data/full_data_assets.txt') as f0, open('data/returns.csv') as f1, open('data/value.csv') as f2:
     full_data_assets = f0.read().split()
@@ -39,26 +40,34 @@ def get_style_return(t_index, all_asset_returns, all_asset_style_betas, gearing_
     
 
 def get_gearing_ratio(asset_style_betas, asset_returns, style_returns):
-    # Create linear regression object
-    regr = linear_model.LinearRegression()
-
     # This is a linear regression with one feature.
     # The independent variable X is the style beta multiplied by return to style.
     x = np.multiply(asset_style_betas[:NUM_T_PERIODS], style_returns[:NUM_T_PERIODS]).reshape(-1, 1)
 
+    # Add a constant term since data is not necessarily centered. Do we need this?
+    x = sm.add_constant(x)
+
     # The dependent variable Y is the return to the asset
-    regr.fit(x, asset_returns[:NUM_T_PERIODS])
+    results = sm.OLS(asset_returns[:NUM_T_PERIODS], x).fit()
+
+    coefficient = results.params[1]
+    standard_error = results.bse[1]
+
+    # If error term for the coefficient is above a certain threshold, then we set the coefficient
+    # to zero, which hopefully excludes it from the style portfolio in next iteration
+    if standard_error > ERROR_THRESHOLD:
+        coefficient = 0
 
     # The estimated coefficient is the gearing ratio
-    return regr.coef_[0]
+    return coefficient
 
 
 # Initialize gearing ratios to 1
 gearing_ratios = [1] * len(returns)
-delta = THRESHOLD
+delta = DELTA_THRESHOLD
 
 # Continue running this exercise until the change in gearing ratios falls below a certain threshold
-while delta >= THRESHOLD:
+while delta >= DELTA_THRESHOLD:
 
     style_returns = [get_style_return(t, returns, style_betas, gearing_ratios) for t in range(NUM_T_PERIODS)]
 
